d3e2da8faa0ab31433b4d22b13fee25159e045b2
JBraunschweig <82617804+JBraunschweig@users.noreply.github.com>
2023-02-09 19:17:01 -0700

    Add files via upload

--- Sockets/WebServer/src/main/java/funHttpServer/WebServer.java
+++ Sockets/WebServer/src/main/java/funHttpServer/WebServer.java
@@ -379,7 +379,7 @@ class WebServer {
                 num2 = num2 * -1;
               }
 
-              if(num1 < num2){
+              if(num1 > num2){
                 int temp = num1;
                 num1 = num2;
                 num2 = temp;
